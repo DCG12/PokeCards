8ed9ec09595f5a0bbf2bfc9a1cbd22ab58638ca5
David <46406163y@iespoblenou.org>
2017-05-12 11:40:19 +0200

    Creacio del listView i del lv_meals_row

--- app/src/main/java/com/example/user/worldmeal/MainActivityFragment.java
+++ app/src/main/java/com/example/user/worldmeal/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvMeals = (ListView) view.findViewById(R.id.lvMeals);
+
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_meals_row,
+                R.id.tvMeal,
+                items
+        );
+        lvMeals.setAdapter(adapter);
+
+        return view;
     }
 }
