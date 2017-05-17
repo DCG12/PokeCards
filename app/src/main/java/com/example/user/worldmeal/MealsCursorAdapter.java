e714f59a3626f1f3be5fe7f71c21eb5ec53d33fe
David <46406163y@iespoblenou.org>
2017-05-17 11:32:41 +0200

    Creación del MealsCursorAdapter

---
+++ app/src/main/java/com/example/user/worldmeal/MealsCursorAdapter.java
@@ -0,0 +1,36 @@
+package com.example.user.worldmeal;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.databinding.DataBindingUtil;
+
+import com.bumptech.glide.Glide;
+import com.example.user.worldmeal.databinding.LvMealsRowBinding;
+
+
+public class MealsCursorAdapter extends CupboardCursorAdapter<Meals> {
+
+    public MealsCursorAdapter(Context context, Class<Meals> entityClass) {
+        super(context, entityClass);
+    }
+
+    @Override
+    public View newView(Context context, Meals model, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        LvMealsRowBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.lv_meals_row, parent, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void bindView(View view, Context context, Meals model) {
+        LvMealsRowBinding binding = DataBindingUtil.getBinding(view);
+        binding.tvMeal.setText(model.getNombre());
+        binding.tvCategory.setText(model.getCategoria());
+        Glide.with(context).load(model.getImagen()).into(binding.ivImage);
+    }
+}
